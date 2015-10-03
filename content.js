275aec073a1ab9ed0d292212eaf2f6b4233975f1
returnexitsuccess <returnexitsuccess@gmail.com>
2015-10-03 00:09:44 -0700

    dynamically replacing course info links

--- content.js
+++ content.js
@@ -1,5 +1,8 @@
 // alert('This code does nothing')
 
-var links = $("a[href*='CourseDetailLink']").replaceWith('<a id="pageContent_CourseList_CourseDetailLink_0" href="test">course info</a>');
-
-console.log(links);
+var links = $("a[href*='CourseDetailLink']");
+for (i = 0; i < links.length; i++)
+	{
+	$("a[id='pageContent_CourseList_CourseDetailLink_".concat(i.toString(), "']")).replaceWith('<a id="pageContent_CourseList_CourseDetailLink_'.concat(i.toString(), '" href="javascript:myFunction(', i.toString(), ')">course info</a>'));
+	}
+//console.log(links);
