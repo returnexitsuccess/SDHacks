004d38d34fcd34b7af754ae3ca9f09c4ea5b29c6
returnexitsuccess <returnexitsuccess@gmail.com>
2015-10-03 01:05:13 -0700

    created onclick event

--- content.js
+++ content.js
@@ -1,8 +1,14 @@
 // alert('This code does nothing')
 
 var links = $("a[href*='CourseDetailLink']");
+var e = []
 for (i = 0; i < links.length; i++)
 	{
-	$("a[id='pageContent_CourseList_CourseDetailLink_".concat(i.toString(), "']")).replaceWith('<a id="pageContent_CourseList_CourseDetailLink_'.concat(i.toString(), '" href="javascript:myFunction(', i.toString(), ')">course info</a>'));
+	//$("a[id='pageContent_CourseList_CourseDetailLink_".concat(i.toString(), "']")).replaceWith('<a id="pageContent_CourseList_CourseDetailLink_'.concat(i.toString(), '" href="javascript:myFunction(', i.toString(), ')">course info</a>'));
+	$("a[id='pageContent_CourseList_CourseDetailLink_".concat(i.toString(), "']")).click( function() { 
+		console.log(event.target.id); 
+		return false; 
+		} );
 	}
 //console.log(links);
+
