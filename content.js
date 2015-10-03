c4485078b35c2ee704acaf81d9d2ee62f15852bd
returnexitsuccess <returnexitsuccess@gmail.com>
2015-10-02 23:36:16 -0700

    replace course info links

--- content.js
+++ content.js
@@ -1,1 +1,5 @@
-alert('This code does nothing')
+// alert('This code does nothing')
+
+var links = $("a[href*='CourseDetailLink']").replaceWith('<a id="pageContent_CourseList_CourseDetailLink_0" href="test">course info</a>');
+
+console.log(links);
